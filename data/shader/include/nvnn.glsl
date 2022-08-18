7050481da2d2b4a36605e8c414471ac836a1f9a3
student-spindler <student-spindler>
2022-08-18 12:50:04 +0200

    Moved Nrc to OldNrc

--- data/shader/include/nvnn.glsl
+++
@@ -1,8 +0,0 @@
-void testNvnn()
-{
-	fcoopmatNV<32, gl_ScopeSubgroup, 8, 8> matA = fcoopmatNV<32, gl_ScopeSubgroup, 8, 8>(0.0);
-	fcoopmatNV<32, gl_ScopeSubgroup, 8, 8> matB = fcoopmatNV<32, gl_ScopeSubgroup, 8, 8>(0.0);
-	fcoopmatNV<32, gl_ScopeSubgroup, 8, 8> matC = fcoopmatNV<32, gl_ScopeSubgroup, 8, 8>(0.0);
-	fcoopmatNV<32, gl_ScopeSubgroup, 8, 8> matD = coopMatMulAddNV(matA, matB, matC);
-	debugPrintfEXT("%f\n", matD[1]);
-}
