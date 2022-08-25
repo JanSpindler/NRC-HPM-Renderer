36b96ecc16dd4e0f72aa2e057cac763e1d55ab98
student-spindler <student-spindler>
2022-08-25 16:40:52 +0200

    forward shader can use coopmat

--- data/shader/include/constants.glsl
+++ data/shader/include/constants.glsl
@@ -1,28 +1,28 @@
-layout(constant_id = 0) const uint RENDER_WIDTH = 0;
-layout(constant_id = 1) const uint RENDER_HEIGHT = 0;
+layout(constant_id = 0) const uint RENDER_WIDTH = 1;
+layout(constant_id = 1) const uint RENDER_HEIGHT = 1;
 
-layout(constant_id = 2) const uint TRAIN_WIDTH = 0;
-layout(constant_id = 3) const uint TRAIN_HEIGHT = 0;
+layout(constant_id = 2) const uint TRAIN_WIDTH = 1;
+layout(constant_id = 3) const uint TRAIN_HEIGHT = 1;
 
-layout(constant_id = 4) const uint POS_ENCODING = 0;
-layout(constant_id = 5) const uint DIR_ENCODING = 0;
+layout(constant_id = 4) const uint POS_ENCODING = 1;
+layout(constant_id = 5) const uint DIR_ENCODING = 1;
 
-layout(constant_id = 6) const uint POS_FREQ_COUNT = 0;
-layout(constant_id = 7) const uint POS_MIN_FREQ = 0;
-layout(constant_id = 8) const uint POS_MAX_FREQ = 0;
-layout(constant_id = 9) const uint POS_LEVEL_COUNT = 0;
-layout(constant_id = 10) const uint POS_HASH_TABLE_SIZE = 0;
-layout(constant_id = 11) const uint POS_FEATURE_COUNt = 0;
+layout(constant_id = 6) const uint POS_FREQ_COUNT = 1;
+layout(constant_id = 7) const uint POS_MIN_FREQ = 1;
+layout(constant_id = 8) const uint POS_MAX_FREQ = 1;
+layout(constant_id = 9) const uint POS_LEVEL_COUNT = 1;
+layout(constant_id = 10) const uint POS_HASH_TABLE_SIZE = 1;
+layout(constant_id = 11) const uint POS_FEATURE_COUNt = 1;
 
-layout(constant_id = 12) const uint DIR_FREQ_COUNT = 0;
-layout(constant_id = 13) const uint DIR_FEATURE_COUNT = 0;
+layout(constant_id = 12) const uint DIR_FREQ_COUNT = 1;
+layout(constant_id = 13) const uint DIR_FEATURE_COUNT = 1;
 
-layout(constant_id = 14) const uint LAYER_COUNT = 0;
-layout(constant_id = 15) const uint LAYER_WIDTH = 0;
-layout(constant_id = 16) const uint INPUT_FEATURE_COUNT = 0;
+layout(constant_id = 14) const uint LAYER_COUNT = 1;
+layout(constant_id = 15) const uint LAYER_WIDTH = 1;
+layout(constant_id = 16) const uint INPUT_FEATURE_COUNT = 1;
 layout(constant_id = 17) const float NRC_LEARNING_RATE = 0.0;
 layout(constant_id = 18) const float MRHE_LEARNING_RATE = 0.0;
-layout(constant_id = 19) const uint BATCH_SIZE = 0;
+layout(constant_id = 19) const uint BATCH_SIZE = 1;
 
 const vec3 skySize = vec3(125.0, 85.0, 153.0) / 2.0;
 const vec3 skyPos = vec3(0.0);
