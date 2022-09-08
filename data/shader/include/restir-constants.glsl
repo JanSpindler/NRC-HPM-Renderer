aff79104fed53955db31a0171e0b8bde98693ee9
student-spindler <student-spindler>
2022-09-08 12:22:45 +0200

    Seperated path generation and shading

--- data/shader/include/restir-constants.glsl
+++ data/shader/include/restir-constants.glsl
@@ -1,7 +1,7 @@
 layout(constant_id = 0) const uint RENDER_WIDTH = 1;
 layout(constant_id = 1) const uint RENDER_HEIGHT = 1;
 
-layout(constant_id = 2) const uint PATH_VERTEX_COUNT = 1;
+layout(constant_id = 2) const uint PATH_VERTEX_COUNT = 2;
 
 const vec3 skySize = vec3(125.0, 85.0, 153.0) / 2.0;
 const vec3 skyPos = vec3(0.0);
