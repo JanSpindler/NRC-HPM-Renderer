90203062cedad4e06c20ac1894b8983195dce262
JanSpindler <JanSpindler>
2022-10-01 10:43:56 +0200

    Added composite encoding to tcnn test

--- src/cuda_main.cu
+++ src/cuda_main.cu
@@ -12,35 +12,51 @@ void RunTcnn()
 		{"otype", "Adam"},
 		{"learning_rate", 1e-3},
 	}},
-	//{"encoding", {
-	//	{"otype", "HashGrid"},
-	//	{"n_levels", 16},
-	//	{"n_features_per_level", 2},
-	//	{"log2_hashmap_size", 19},
-	//	{"base_resolution", 16},
-	//	{"per_level_scale", 2.0},
-	//}},
+	{"encoding", {
+		{"otype", "Composite"},
+		{"reduction", "Concatenation"},
+		{"nested", {
+			{
+				{"otype", "HashGrid"},
+				{"n_dims_to_encode", 3},
+				{"n_levels", 16},
+				{"n_features_per_level", 2},
+				{"log2_hashmap_size", 19},
+				{"base_resolution", 16},
+				{"per_level_scale", 2.0},
+			},
+			{
+				{"otype", "OneBlob"},
+				{"n_dims_to_encode", 2},
+				{"n_bins", 4},
+			},
+		}},
+	}},
 	{"network", {
 		{"otype", "FullyFusedMLP"},
 		{"activation", "ReLU"},
 		{"output_activation", "None"},
-		{"n_neurons", 64},
-		{"n_hidden_layers", 2},
+		{"n_neurons", 128},
+		{"n_hidden_layers", 6},
 	}},
 	};
 
-	const uint32_t n_input_dims = 32;
-	const uint32_t n_output_dims = 64;
+	const uint32_t n_input_dims = 5;
+	const uint32_t n_output_dims = 3;
+	const uint32_t n_inference_steps = 36;
 	const uint32_t n_training_steps = 10;
-	const uint32_t batch_size = 128;
+	const uint32_t batch_size = 16384;
 
-	auto model = tcnn::create_from_config(n_input_dims, n_output_dims, config);
+	tcnn::TrainableModel model = tcnn::create_from_config(n_input_dims, n_output_dims, config);
 
 	tcnn::GPUMatrix<float> training_batch_inputs(n_input_dims, batch_size);
 	tcnn::GPUMatrix<float> training_batch_targets(n_output_dims, batch_size);
 
-	// Use the model
 	tcnn::GPUMatrix<float> inference_inputs(n_input_dims, batch_size);
 	tcnn::GPUMatrix<float> inference_outputs(n_output_dims, batch_size);
-	model.network->inference(inference_inputs, inference_outputs);
+	
+	for (uint32_t i = 0; i < n_inference_steps; i++)
+	{
+		model.network->inference(inference_inputs, inference_outputs);
+	}
 }
