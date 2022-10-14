bdbefbbe59e118a68b04b4f7288692b17c4a3937
JanSpindler <JanSpindler>
2022-10-14 10:29:56 +0200

    NrcHpmRenderer shaders compile without errors

--- data/shader/include/nrc-descriptors.glsl
+++ data/shader/include/nrc-descriptors.glsl
@@ -31,103 +31,47 @@ layout(set = 2, binding = 0) uniform dir_light_t
 	float strength;
 } dir_light;
 
-// NN buffers
-layout(std430, set = 3, binding = 0) buffer RenderNeurons
-{
-	float16_t renderNeurons[];
-};
-
-layout(std430, set = 3, binding = 1) buffer TrainNeurons
-{
-	float16_t trainNeurons[];
-};
-
-layout(std430, set = 3, binding = 2) buffer Weights
-{
-	float16_t weights[];
-};
-
-layout(std430, set = 3, binding = 3) buffer DeltaWeights
-{
-	float16_t deltaWeights[];
-};
-
-layout(std430, set = 3, binding = 4) buffer MomentumWeights
-{
-	float16_t momentumWeights[];
-};
-
-layout(std430, set = 3, binding = 5) buffer Biases
-{
-	float16_t biases[];
-};
-
-layout(std430, set = 3, binding = 6) buffer DeltaBiases
-{
-	float16_t deltaBiases[];
-};
-
-layout(std430, set = 3, binding = 7) buffer MomentumBiases
-{
-	float16_t momentumBiases[];
-};
-
-layout(std430, set = 3, binding = 8) buffer Mrhe
-{
-	float16_t mrhe[];
-};
-
-layout(std430, set = 3, binding = 9) buffer DeltaMrhe
-{
-	float16_t deltaMrhe[];
-};
-
-layout(std430, set = 3, binding = 10) buffer MrheRes
-{
-	uint mrheResolutions[];
-};
-
-layout(set = 4, binding = 0) uniform PointLight
+layout(set = 3, binding = 0) uniform PointLight
 {
 	vec3 pos;
 	float strength;
 	vec3 color;
 } pointLight;
 
-layout(set = 5, binding = 0) uniform sampler2D hdrEnvMap;
+layout(set = 4, binding = 0) uniform sampler2D hdrEnvMap;
 
-layout(set = 5, binding = 1) uniform sampler2D hdrEnvMapInvCdfX;
+layout(set = 4, binding = 1) uniform sampler2D hdrEnvMapInvCdfX;
 
-layout(set = 5, binding = 2) uniform sampler1D hdrEnvMapInvCdfY;
+layout(set = 4, binding = 2) uniform sampler1D hdrEnvMapInvCdfY;
 
-layout(set = 5, binding = 3) uniform HdrEnvMapData
+layout(set = 4, binding = 3) uniform HdrEnvMapData
 {
 	float directStrength;
 	float hpmStrength;
 } hdrEnvMapData;
 
-layout(set = 6, binding = 0, rgba32f) uniform image2D nrcOutputImage;
+layout(set = 5, binding = 0, rgba32f) uniform image2D nrcOutputImage;
 
-layout(set = 6, binding = 1, rgba32f) uniform image2D nrcPrimaryRayColorImage;
+layout(set = 5, binding = 1, rgba32f) uniform image2D nrcPrimaryRayColorImage;
 
-layout(set = 6, binding = 2, rgba32f) uniform image2D nrcPrimaryRayInfoImage;
+layout(set = 5, binding = 2, rgba32f) uniform image2D nrcPrimaryRayInfoImage;
 
-layout(set = 6, binding = 3) buffer NrcInferInput
+layout(set = 5, binding = 3) buffer NrcInferInput
 {
 	float nrcInferInput[];
 };
 
-layout(set = 6, binding = 4) buffer NrcInferOutput
+layout(set = 5, binding = 4) buffer NrcInferOutput
 {
 	float nrcInferOutput[];
 };
 
-layout(set = 6, binding = 5) buffer NrcTrainInput
+layout(set = 5, binding = 5) buffer NrcTrainInput
 {
 	float nrcTrainInput[];
 };
 
-layout(set = 6, binding = 6) buffer NrcTrainTarget
+layout(set = 5, binding = 6) buffer NrcTrainTarget
 {
 	float nrcTrainTarget[];
 };
