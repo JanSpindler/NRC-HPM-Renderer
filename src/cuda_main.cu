10ce8a6046e3c85b032694b693e38bd8cb557802
JanSpindler <JanSpindler>
2022-12-03 16:42:23 +0100

    Dividing mse by 3 for color channels

--- src/cuda_main.cu
+++ src/cuda_main.cu
@@ -106,7 +106,7 @@ void SwapchainResizeCallback()
 void Benchmark(const en::Camera* camera, VkQueue queue, size_t frameCount)
 {
 	en::Log::Info("Frame: " + std::to_string(frameCount));
-	//reference->CompareNrc(*nrcHpmRenderer, camera, queue);
+	reference->CompareNrc(*nrcHpmRenderer, camera, queue);
 	//reference->CompareMc(*mcHpmRenderer, camera, queue);
 }
 
