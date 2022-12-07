550e35c3a8e495456aef728ee7f751541e3e05e3
JanSpindler <JanSpindler>
2022-12-07 14:38:29 +0100

    Added classes for acceleration structure (further work later)

---
+++ include/engine/graphics/AccelerationStructure.hpp
@@ -0,0 +1,18 @@
+/*#pragma once
+
+#include <engine/graphics/common.hpp>
+#include <engine/objects/Model.hpp>
+
+namespace en
+{
+	class BotLevAccStruct
+	{
+	public:
+		BotLevAccStruct(const Model& model);
+
+		void Destroy();
+
+	private:
+	};
+}
+*/
