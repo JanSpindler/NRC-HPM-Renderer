30ce9dea95cefa552759c9ec46fd73c0e3c7fd03
JanSpindler <JanSpindler>
2023-08-05 13:55:53 +0200

    openvdb build script works

--- install-openvdb.bat
+++ install-openvdb.bat
@@ -1,6 +1,5 @@
 cd openvdb
 if not exist build mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX="../../OpenVDBInstall" -A x64 ..
-cmake --build . -j --config Debug --target install
-cmake --build . -j --config Release --target install
+cmake -DCMAKE_INSTALL_PREFIX:PATH="../../openvdb-install" -A x64 ..
+cmake --build . --parallel 16 --config Release --target install
